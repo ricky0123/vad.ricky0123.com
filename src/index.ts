b318091d6a7c6eb78e947a95b6815f555b7483e6
ricky <rickycontact9@gmail.com>
2022-11-26 09:45:29 -0600

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,80 @@
 import "./main.css"
 import Alpine from "alpinejs"
 import * as vad from "@ricky0123/vad"
-let windowAny = window as any
-windowAny.Alpine = Alpine
+let _window = window as any
+_window.Alpine = Alpine
 Alpine.start()
 
+enum DemoState {
+  before = "before",
+  during = "during",
+  loading = "loading",
+  errored = "errored",
+}
+
+const updateDemoState = (newState: DemoState) => {
+  const ev = new CustomEvent("demo-state-change", {
+    detail: { demoState: newState },
+  })
+  window.dispatchEvent(ev)
+}
+
+let myvad: vad.MicVAD
+
+function updateIndicatorColor(
+  isSpeech: number,
+  positiveSpeechThreshold: number,
+  element: HTMLElement
+) {
+  const current = element.style.getPropertyValue("--color")
+  if (isSpeech > positiveSpeechThreshold && current != "red") {
+    element.style.setProperty("--color", "red")
+  } else if (isSpeech < positiveSpeechThreshold && current != "blue") {
+    element.style.setProperty("--color", "blue")
+  }
+}
+
+function updateIndicatorSpeed(
+  isSpeech: number,
+  positiveSpeechThreshold: number,
+  element: HTMLElement
+) {
+  const lower = `0.1s`
+  const upper = `0.75s`
+  const current = element.style.getPropertyValue("--speed")
+  if (isSpeech > positiveSpeechThreshold && current != lower) {
+    element.style.setProperty("--speed", lower)
+  } else if (isSpeech < positiveSpeechThreshold && current != upper) {
+    element.style.setProperty("--speed", upper)
+  }
+}
+
 async function startDemo() {
-  console.log("starting demo...")
+  updateDemoState(DemoState.loading)
+
   try {
-    const myvad = await vad.MicVAD.new({
+    let counter = 0
+    const indicatorRefreshRate = 4
+    myvad = await vad.MicVAD.new({
       positiveSpeechThreshold: 0.8,
       minSpeechFrames: 5,
       preSpeechPadFrames: 10,
-      onFrameProcessed: (probs) => {},
+      onFrameProcessed: (probs) => {
+        if (counter % indicatorRefreshRate == 0) {
+          const indicator: HTMLElement = document.querySelector("#indicator")
+          updateIndicatorColor(
+            probs.isSpeech,
+            myvad.options.positiveSpeechThreshold,
+            indicator
+          )
+          updateIndicatorSpeed(
+            probs.isSpeech,
+            myvad.options.positiveSpeechThreshold,
+            indicator
+          )
+        }
+        counter = (counter + 1) % indicatorRefreshRate
+      },
       onSpeechEnd: (audio) => {
         const wavBuffer = vad.utils.encodeWAV(audio)
         const base64 = vad.utils.arrayBufferToBase64(wavBuffer)
@@ -29,20 +91,23 @@ async function startDemo() {
         playlist.prepend(entry)
       },
     })
-    console.log("loaded vad")
-    windowAny.myvad = myvad
+    _window.myvad = myvad
 
-    windowAny.toggleVAD = () => {
-      console.log("ran toggle vad")
+    _window.toggleVAD = () => {
       if (myvad.listening === false) {
         myvad.start()
       } else {
         myvad.pause()
       }
     }
-    windowAny.toggleVAD()
+    _window.toggleVAD()
+    updateDemoState(DemoState.during)
   } catch (e) {
     console.error("Failed:", e)
+    if (myvad !== undefined) {
+      myvad.pause()
+    }
+    if (myvad !== undefined) updateDemoState(DemoState.errored)
   }
 }
-windowAny.startDemo = startDemo
+_window.startDemo = startDemo
